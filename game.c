874b514a72e3b849698f08743a196df53386a2bb
Alexis TALIN <123313705+LeafPuls@users.noreply.github.com>
2026-04-01 20:48:30 +0200

    01/04 20.48

--- game.c
+++ game.c
@@ -16,16 +16,6 @@
 
 
 //=====Initialisation de la matrice de jeu=====
-void init_plat()
-{
-	int i, j;
-	for (i = 0; i < 5; i++)
-	{
-		for (j = 0; j < 5; j++)
-		{
-			plat1[i][j] = '0';//vide
-			plat2[i][j] = '0';//vide
-		}
-	}
-}
+
+
 
